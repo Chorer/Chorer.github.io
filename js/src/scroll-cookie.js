c5af6522120e9407497d518b48b04caf3030d0ad
Chorer <zcl82806932@163.com>
2019-03-04 13:35:58 +0800

    Site updated: 2019-03-04 13:35:53

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 03 2019 12:59:08 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 04 2019 13:35:16 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 

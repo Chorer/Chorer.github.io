c431bca7fcdf0467470d2992ccf37959749049b1
Chorer <zcl82806932@163.com>
2019-03-24 17:25:38 +0800

    Site updated: 2019-03-24 17:25:36

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 24 2019 15:05:54 GMT+0800 (GMT+08:00)
+// build time:Sun Mar 24 2019 17:25:27 GMT+0800 (GMT+08:00)
 (function(o){"use strict";o.fn.toTop=function(t){var i=this;var e=o(window);var s=o("html, body");var n=o.extend({autohide:true,offset:420,speed:500,position:true,right:15,bottom:30},t);i.css({cursor:"pointer"});if(n.autohide){i.css("display","none")}if(n.position){i.css({position:"fixed",right:n.right,bottom:n.bottom})}i.click(function(){s.animate({scrollTop:0},n.speed)});e.scroll(function(){var o=e.scrollTop();if(n.autohide){if(o>n.offset){i.fadeIn(n.speed)}else i.fadeOut(n.speed)}})}})(jQuery);$(".to-top").toTop({offset:100,right:0,bottom:242});
 //rebuild by neat 
--- js/tagscloud.js
+++ js/tagscloud.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 24 2019 15:05:54 GMT+0800 (GMT+08:00)
+// build time:Sun Mar 24 2019 17:25:27 GMT+0800 (GMT+08:00)
 $(".tagscloud a").addClass("tags-a");$(".tagscloud a:last-child").removeClass("tags-a");
 //rebuild by neat 

afaafafeb5ab6970305ae6f7ce31befdc99f0c4e
Chorer <zcl82806932@163.com>
2019-03-27 22:52:46 +0800

    Site updated: 2019-03-27 22:52:44

--- js/tagscloud.js
+++ js/tagscloud.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 27 2019 00:35:34 GMT+0800 (GMT+08:00)
+// build time:Wed Mar 27 2019 22:52:32 GMT+0800 (GMT+08:00)
 $(".tagscloud a").addClass("tags-a");$(".tagscloud a:last-child").removeClass("tags-a");
 //rebuild by neat 

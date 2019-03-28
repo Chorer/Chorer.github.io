7ec413290b548ec581e2344ba3eed3ff7a3d0977
Chorer <zcl82806932@163.com>
2019-03-28 13:04:03 +0800

    Site updated: 2019-03-28 13:04:00

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 12:55:38 GMT+0800 (GMT+08:00)
-var myNav=document.getElementsByClassName("menu")[0].getElementsByClassName("menu-item-link");var myURL=document.location.href;for(var i=1;i<myNav.length;i++){var links=myNav[i].getAttribute("href");if(myURL.indexOf(links)!=-1){myNav[i].className="menu-item-link2"}}
+// build time:Thu Mar 28 2019 13:03:37 GMT+0800 (GMT+08:00)
+var myNav=document.getElementsByClassName("menu")[0].getElementsByClassName("menu-item-link");var myURL=document.location.href;for(var i=1;i<myNav.length;i++){var links=myNav[i].getAttribute("href");if(myURL.indexOf(links)!=-1){myNav[i].className="menu-item-link2"}}var home_link="https://chorer.github.io";if(1){myNav[0].className="menu-item-link2"}
 //rebuild by neat 

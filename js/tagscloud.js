ad66970819f19a287bd161864c6ec39aff552dec
Chorer <zcl82806932@163.com>
2019-03-30 20:40:55 +0800

    Site updated: 2019-03-30 20:40:51

--- js/tagscloud.js
+++ js/tagscloud.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 30 2019 17:08:51 GMT+0800 (GMT+08:00)
+// build time:Sat Mar 30 2019 20:40:44 GMT+0800 (GMT+08:00)
 $(".tagscloud a").addClass("tags-a");$(".tagscloud a:last-child").removeClass("tags-a");
 //rebuild by neat 
--- js/toc.js
+++ js/toc.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 30 2019 17:08:51 GMT+0800 (GMT+08:00)
+// build time:Sat Mar 30 2019 20:40:44 GMT+0800 (GMT+08:00)
 var oDiv=document.getElementsByClassName("toc")[0],H=0,Y=oDiv;while(Y){H+=Y.offsetTop;Y=Y.offsetParent}window.onscroll=function(){var e=document.body.scrollTop||document.documentElement.scrollTop;if(e>H){oDiv.style="position:fixed;top:-18px;"}else{oDiv.style=""}};var txt=document.createElement("li");oDiv.insertBefore(txt,oDiv.children[0]);txt.className="list_title";txt.innerHTML="目录";var topper=document.createElement("li");topper.className="toTop";topper.innerHTML="To the Top";oDiv.appendChild(topper);
 //rebuild by neat 

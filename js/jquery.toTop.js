92305ea5ff8a76bff902efa1ee2178e68009a63e
Chorer <zcl82806932@163.com>
2019-03-30 21:19:08 +0800

    Site updated: 2019-03-30 21:19:03

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 30 2019 20:40:44 GMT+0800 (GMT+08:00)
+// build time:Sat Mar 30 2019 20:41:27 GMT+0800 (GMT+08:00)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toTop").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 
--- js/tagscloud.js
+++ js/tagscloud.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 30 2019 20:40:44 GMT+0800 (GMT+08:00)
+// build time:Sat Mar 30 2019 20:41:27 GMT+0800 (GMT+08:00)
 $(".tagscloud a").addClass("tags-a");$(".tagscloud a:last-child").removeClass("tags-a");
 //rebuild by neat 

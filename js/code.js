3efe488584a45ff12f3ad89ced3b0bf0a280b9e7
Chorer <zcl82806932@163.com>
2019-05-10 21:55:39 +0800

    Site updated: 2019-05-10 21:55:33

--- js/code.js
+++ js/code.js
@@ -1,3 +1,3 @@
-// build time:Fri May 10 2019 12:47:08 GMT+0800 (GMT+08:00)
+// build time:Fri May 10 2019 21:54:59 GMT+0800 (GMT+08:00)
 var codes=document.getElementsByTagName("code");for(var i=0;i<codes.length;i++){if(codes[i].parentNode.nodeName!="PRE"){codes[i].classList.add("littleCode")}}
 //rebuild by neat 
--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Fri May 10 2019 12:47:08 GMT+0800 (GMT+08:00)
+// build time:Fri May 10 2019 21:54:59 GMT+0800 (GMT+08:00)
 $(function(){var t=500;var o=document.body.clientHeight;$(".real-toTop-text").click(function(){$("html,body").animate({scrollTop:"0px"},t)})});
 //rebuild by neat 

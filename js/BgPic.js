29bcf2c343562cc6837640cee1d02b8a17a2256a
Chorer <zcl82806932@163.com>
2019-05-14 12:33:36 +0800

    Site updated: 2019-05-14 12:33:34

--- js/BgPic.js
+++ js/BgPic.js
@@ -1,3 +1,3 @@
-// build time:Tue May 14 2019 12:17:57 GMT+0800 (GMT+08:00)
+// build time:Tue May 14 2019 12:33:29 GMT+0800 (GMT+08:00)
 var header=document.getElementsByClassName("header")[0];var footer=document.getElementsByClassName("footer")[0];var randomBgIndex=Math.round(Math.random()*2);var arrays_header=["header1","header2","header3"];var arrays_footer=["footer1","footer2","footer3"];header.classList.add(arrays_header[randomBgIndex]);footer.classList.add(arrays_footer[randomBgIndex]);
 //rebuild by neat 
--- js/code.js
+++ js/code.js
@@ -1,3 +1,3 @@
-// build time:Tue May 14 2019 12:17:57 GMT+0800 (GMT+08:00)
+// build time:Tue May 14 2019 12:33:29 GMT+0800 (GMT+08:00)
 var codes=document.getElementsByTagName("code");for(var i=0;i<codes.length;i++){if(codes[i].parentNode.nodeName!="PRE"){codes[i].classList.add("littleCode")}}
 //rebuild by neat 

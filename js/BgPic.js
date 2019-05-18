2d8f945c7fb5a1bb78c092c25c6d60cf8c641aa9
Chorer <zcl82806932@163.com>
2019-05-18 17:26:37 +0800

    Site updated: 2019-05-18 17:26:33

--- js/BgPic.js
+++ js/BgPic.js
@@ -1,3 +1,3 @@
-// build time:Sat May 18 2019 17:24:26 GMT+0800 (GMT+08:00)
+// build time:Sat May 18 2019 17:26:33 GMT+0800 (GMT+08:00)
 var header=document.getElementsByClassName("header")[0];var footer=document.getElementsByClassName("footer")[0];var randomBgIndex=Math.round(Math.random()*3);var arrays_header=["header1","header2","header3","header4"];var arrays_footer=["footer1","footer2","footer3","footer4"];header.classList.add(arrays_header[randomBgIndex]);footer.classList.add(arrays_footer[randomBgIndex]);
 //rebuild by neat 
--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sat May 18 2019 17:24:26 GMT+0800 (GMT+08:00)
+// build time:Sat May 18 2019 17:26:33 GMT+0800 (GMT+08:00)
 $(function(){var t=500;var o=document.body.clientHeight;$(".real-toTop-text").click(function(){$("html,body").animate({scrollTop:"0px"},t)})});
 //rebuild by neat 

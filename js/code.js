8fa11a18da296f376078cd804a8faadd8ab93c5c
Chorer <zcl82806932@163.com>
2019-05-18 17:22:17 +0800

    Site updated: 2019-05-18 17:22:11

--- js/code.js
+++ js/code.js
@@ -1,3 +1,3 @@
-// build time:Tue May 14 2019 20:04:54 GMT+0800 (GMT+08:00)
+// build time:Sat May 18 2019 17:21:35 GMT+0800 (GMT+08:00)
 var codes=document.getElementsByTagName("code");for(var i=0;i<codes.length;i++){if(codes[i].parentNode.nodeName!="PRE"){codes[i].classList.add("littleCode")}}
 //rebuild by neat 

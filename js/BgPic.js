1248e774d0c8707b3b5a4d4f98079e07798d7313
Chorer <zcl82806932@163.com>
2019-05-20 21:30:07 +0800

    Site updated: 2019-05-20 21:30:05

--- js/BgPic.js
+++ js/BgPic.js
@@ -1,3 +1,3 @@
-// build time:Mon May 20 2019 21:05:16 GMT+0800 (GMT+08:00)
+// build time:Mon May 20 2019 21:21:12 GMT+0800 (GMT+08:00)
 var header=document.getElementsByClassName("header")[0];var footer=document.getElementsByClassName("footer")[0];var randomBgIndex=Math.round(Math.random()*8);var arrays_header=["header1","header2","header3","header4","header5","header6","header7","header8","header9"];var arrays_footer=["footer1","footer2","footer3","footer4","footer5","footer6","footer7","footer8","footer9"];header.classList.add(arrays_header[randomBgIndex]);footer.classList.add(arrays_footer[randomBgIndex]);
 //rebuild by neat 

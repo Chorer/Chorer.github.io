febd14cb3e67e55610f99c83c6d3159f8ee8f6fb
Chorer <zcl82806932@163.com>
2019-05-20 20:59:38 +0800

    Site updated: 2019-05-20 20:59:36

--- js/code.js
+++ js/code.js
@@ -1,3 +1,3 @@
-// build time:Mon May 20 2019 19:36:29 GMT+0800 (GMT+08:00)
+// build time:Mon May 20 2019 20:59:14 GMT+0800 (GMT+08:00)
 var codes=document.getElementsByTagName("code");for(var i=0;i<codes.length;i++){if(codes[i].parentNode.nodeName!="PRE"){codes[i].classList.add("littleCode")}}var text='<svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle><circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle><circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle></g></svg>';var blocks=document.getElementsByClassName("line-numbers");for(var i=0;i<blocks.length;i++){var dot=document.createElement("span");dot.innerHTML=text;dot.className="dot";blocks[i].insertBefore(dot,blocks[i].children[0])}
 //rebuild by neat 

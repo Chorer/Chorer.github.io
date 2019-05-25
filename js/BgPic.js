c429fa71c4d258c528025c724d0a3148b676dd09
Chorer <zcl82806932@163.com>
2019-05-26 00:35:13 +0800

    Site updated: 2019-05-26 00:35:10

--- js/BgPic.js
+++ js/BgPic.js
@@ -1,3 +1,3 @@
-// build time:Sat May 25 2019 19:52:43 GMT+0800 (GMT+08:00)
-var header=document.getElementsByClassName("header")[0];var footer=document.getElementsByClassName("footer")[0];var randomBgIndex=Math.round(Math.random()*8);var arrays_header=["header1","header2","header3","header4","header5","header6","header7","header8","header9"];var arrays_footer=["footer1","footer2","footer3","footer4","footer5","footer6","footer7","footer8","footer9"];header.classList.add(arrays_header[randomBgIndex]);footer.classList.add(arrays_footer[randomBgIndex]);
+// build time:Sun May 26 2019 00:34:34 GMT+0800 (GMT+08:00)
+var header=document.getElementsByClassName("header")[0];var footer=document.getElementsByClassName("footer")[0];var randomBgIndex=Math.round(Math.random()*6);var arrays_header=["header1","header2","header3","header4","header5","header6","header7"];var arrays_footer=["footer1","footer2","footer3","footer4","footer5","footer6","footer7"];header.classList.add(arrays_header[randomBgIndex]);footer.classList.add(arrays_footer[randomBgIndex]);
 //rebuild by neat 
--- js/IndexToTop.js
+++ js/IndexToTop.js
@@ -1,3 +1,3 @@
-// build time:Sat May 25 2019 19:52:43 GMT+0800 (GMT+08:00)
+// build time:Sun May 26 2019 00:34:34 GMT+0800 (GMT+08:00)
 addBackToTop({diameter:56,backgroundColor:"#53b3fa",textColor:"#fff",scrollDuration:300});
 //rebuild by neat 

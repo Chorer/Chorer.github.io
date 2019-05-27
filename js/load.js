25d7dbdfb1b9dfef2a41de2088aebb173f051c0a
Chorer <zcl82806932@163.com>
2019-05-27 12:33:20 +0800

    Site updated: 2019-05-27 12:33:17

--- js/load.js
+++ js/load.js
@@ -1,3 +1,3 @@
-// build time:Mon May 27 2019 12:14:42 GMT+0800 (GMT+08:00)
+// build time:Mon May 27 2019 12:33:10 GMT+0800 (GMT+08:00)
 var isMobile={Android:function(){return navigator.userAgent.match(/Android/i)},BlackBerry:function(){return navigator.userAgent.match(/BlackBerry/i)},iOS:function(){return navigator.userAgent.match(/iPhone|iPad|iPod/i)},Opera:function(){return navigator.userAgent.match(/Opera Mini/i)},Windows:function(){return navigator.userAgent.match(/IEMobile/i)},any:function(){return isMobile.Android()||isMobile.BlackBerry()||isMobile.iOS()||isMobile.Opera()||isMobile.Windows()}};if($(".local-search").length&&!isMobile.any()){$.getScript("/js/search.js",function(){searchFunc("/search.xml","local-search-input","local-search-result")})}
 //rebuild by neat 
--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,3 @@
-// build time:Mon May 27 2019 12:14:42 GMT+0800 (GMT+08:00)
+// build time:Mon May 27 2019 12:33:10 GMT+0800 (GMT+08:00)
 var myNav=document.getElementsByClassName("menu")[0].getElementsByClassName("menu-item-link");var myURL=document.location.href;var home_link_text=document.getElementById("homelink").getAttribute("data-link");if(myURL==home_link_text||myURL.indexOf("page")!=-1){myNav[0].className="menu-item-link2"}else{myNav[0].className="menu-item-link";for(var i=1;i<myNav.length;i++){var links=myNav[i].getAttribute("href");if(myURL.indexOf(links)!=-1){myNav[i].className="menu-item-link2"}}}
 //rebuild by neat 

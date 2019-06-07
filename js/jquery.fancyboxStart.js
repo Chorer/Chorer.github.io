4e9b4f70b34e37486f351d49e3cbcd83ed3fe6b3
Chorer <zcl82806932@163.com>
2019-06-07 20:51:38 +0800

    Site updated: 2019-06-07 20:51:33

--- js/jquery.fancyboxStart.js
+++ js/jquery.fancyboxStart.js
@@ -1,3 +1,3 @@
-// build time:Thu Jun 06 2019 22:22:41 GMT+0800 (GMT+08:00)
+// build time:Fri Jun 07 2019 20:51:08 GMT+0800 (GMT+08:00)
 $(document).ready(function(){FancyBoxStart()});function FancyBoxStart(){$("img").each(function(){var a=$(this);var t=a.parent("a");if(t.length==0){var e=a.attr("src");var r=e.lastIndexOf("?");if(r!=-1){e=e.substring(0,r)}t=a.wrap('<a href="'+e+'"></a>').parent("a")}t.attr("data-fancybox","images")});$().fancybox({selector:'[data-fancybox="images"]',thumbs:false,hash:true,loop:false,fullScreen:false,slideShow:false,protect:true})}
 //rebuild by neat 

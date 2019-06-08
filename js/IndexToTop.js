b7ebbaff93786dd661a49b6dfa85b724b95ea653
Chorer <zcl82806932@163.com>
2019-06-08 23:34:23 +0800

    Site updated: 2019-06-08 23:34:19

--- js/IndexToTop.js
+++ js/IndexToTop.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 08 2019 19:51:36 GMT+0800 (GMT+08:00)
+// build time:Sat Jun 08 2019 23:33:34 GMT+0800 (GMT+08:00)
 addBackToTop({diameter:56,backgroundColor:"#53b3fa",textColor:"#fff",scrollDuration:300});
 //rebuild by neat 
--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 08 2019 19:51:36 GMT+0800 (GMT+08:00)
+// build time:Sat Jun 08 2019 23:33:34 GMT+0800 (GMT+08:00)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 

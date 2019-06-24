75647e655d4c932abe279631687d721b1c3dad2c
Chorer <zcl82806932@163.com>
2019-06-24 21:29:37 +0800

    Site updated: 2019-06-24 21:29:30

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sun Jun 23 2019 21:15:05 GMT+0800 (GMT+08:00)
+// build time:Mon Jun 24 2019 21:29:21 GMT+0800 (GMT+08:00)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 
--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,3 @@
-// build time:Sun Jun 23 2019 21:15:05 GMT+0800 (GMT+08:00)
+// build time:Mon Jun 24 2019 21:29:21 GMT+0800 (GMT+08:00)
 var myNav=document.getElementsByClassName("menu")[0].getElementsByClassName("menu-item-link");var myURL=document.location.href;var home_link_text=document.getElementById("homelink").getAttribute("data-link");if(myURL==home_link_text||myURL.indexOf("page")!=-1){myNav[0].className="menu-item-link2"}else{myNav[0].className="menu-item-link";for(var i=1;i<myNav.length;i++){var links=myNav[i].getAttribute("href");if(myURL.indexOf(links)!=-1){myNav[i].className="menu-item-link2"}}}
 //rebuild by neat 

3e58e6a340b8b063f90db0593a495cd6dfc5e252
Chorer <zcl82806932@163.com>
2019-07-03 14:33:41 +0800

    Site updated: 2019-07-03 14:33:33

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,3 @@
-// build time:Wed Jul 03 2019 13:45:03 GMT+0800 (GMT+08:00)
+// build time:Wed Jul 03 2019 14:32:28 GMT+0800 (GMT+08:00)
 var myNav=document.getElementsByClassName("menu")[0].getElementsByClassName("menu-item-link");var myURL=document.location.href;var home_link_text=document.getElementById("homelink").getAttribute("data-link");if(myURL==home_link_text||myURL.indexOf("page")!=-1){myNav[0].className="menu-item-link2"}else{myNav[0].className="menu-item-link";for(var i=1;i<myNav.length;i++){var links=myNav[i].getAttribute("href");if(myURL.indexOf(links)!=-1){myNav[i].className="menu-item-link2"}}}
 //rebuild by neat 

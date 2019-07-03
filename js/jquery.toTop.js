3f17d567bd0d458937dcb888ee5e1c342fa4050b
Chorer <zcl82806932@163.com>
2019-07-03 13:47:01 +0800

    Site updated: 2019-07-03 13:46:53

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Tue Jul 02 2019 14:45:20 GMT+0800 (GMT+08:00)
+// build time:Wed Jul 03 2019 13:45:03 GMT+0800 (GMT+08:00)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 

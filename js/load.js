9526d67c2803b2edc1257a06379cf23e85a5c316
Chorer <zcl82806932@163.com>
2019-09-20 13:57:00 +0800

    Site updated: 2019-09-20 13:56:56

--- js/load.js
+++ js/load.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 16 2019 18:41:36 GMT+0800 (GMT+08:00)
+// build time:Fri Sep 20 2019 13:48:31 GMT+0800 (GMT+08:00)
 var isMobile={Android:function(){return navigator.userAgent.match(/Android/i)},BlackBerry:function(){return navigator.userAgent.match(/BlackBerry/i)},iOS:function(){return navigator.userAgent.match(/iPhone|iPad|iPod/i)},Opera:function(){return navigator.userAgent.match(/Opera Mini/i)},Windows:function(){return navigator.userAgent.match(/IEMobile/i)},any:function(){return isMobile.Android()||isMobile.BlackBerry()||isMobile.iOS()||isMobile.Opera()||isMobile.Windows()}};if($(".local-search").length&&!isMobile.any()){$.getScript("/js/search.js",function(){searchFunc("/search.xml","local-search-input","local-search-result")})}
 //rebuild by neat 

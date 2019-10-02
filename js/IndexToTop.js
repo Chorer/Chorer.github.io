4acf1cc62937027fc158af92b505b6006e77318e
Chorer <zcl82806932@163.com>
2019-10-02 21:57:19 +0800

    Site updated: 2019-10-02 21:57:16

--- js/IndexToTop.js
+++ js/IndexToTop.js
@@ -1,3 +1,3 @@
-// build time:Wed Oct 02 2019 21:09:21 GMT+0800 (GMT+08:00)
+// build time:Wed Oct 02 2019 21:57:14 GMT+0800 (GMT+08:00)
 addBackToTop({diameter:56,backgroundColor:"#53b3fa",textColor:"#fff",scrollDuration:300});
 //rebuild by neat 

aec03658be78d2bf3e3f1cd8dbd130d9b0cfe0ab
Chorer <zcl82806932@163.com>
2019-10-24 12:04:55 +0800

    Site updated: 2019-10-24 12:04:49

--- js/IndexToTop.js
+++ js/IndexToTop.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 22 2019 14:48:38 GMT+0800 (GMT+08:00)
+// build time:Thu Oct 24 2019 12:04:48 GMT+0800 (GMT+08:00)
 addBackToTop({diameter:56,backgroundColor:"#53b3fa",textColor:"#fff",scrollDuration:300});
 //rebuild by neat 
---
+++ js/post-except.js
@@ -0,0 +1,3 @@
+// build time:Thu Oct 24 2019 12:04:48 GMT+0800 (GMT+08:00)
+var cards=document.querySelectorAll(".post-content");for(var j=0;j<cards.length;j++){if(cards[j].querySelectorAll("img").length>0){cards[j].classList.add("post-content-padding")}if(cards[j].querySelectorAll("blockquote").length>0){cards[j].classList.add("post-content-quote")}}
+//rebuild by neat 

500373cb5fc251d85b4c71c591d3785fc3ee61be
Chorer <zcl82806932@163.com>
2019-11-02 10:33:33 +0800

    Site updated: 2019-11-02 10:33:29

--- js/post-except.js
+++ js/post-except.js
@@ -1,3 +1,3 @@
-// build time:Fri Nov 01 2019 23:41:02 GMT+0800 (GMT+08:00)
+// build time:Sat Nov 02 2019 10:33:28 GMT+0800 (GMT+08:00)
 var cards=document.querySelectorAll(".post-content");for(var j=0;j<cards.length;j++){if(cards[j].querySelectorAll("img").length>0){cards[j].classList.add("post-content-padding")}else{cards[j].classList.add("post-content-noimg")}if(cards[j].querySelectorAll("blockquote").length>0){cards[j].classList.add("post-content-quote")}if(!cards[j].querySelectorAll("p").length){cards[j].children[0].style.display="block"}}
 //rebuild by neat 

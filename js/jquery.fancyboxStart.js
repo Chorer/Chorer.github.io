fcb481b70552174abf345ea4eca08092169a7084
Chorer <zcl82806932@163.com>
2019-12-07 14:55:53 +0800

    Site updated: 2019-12-07 14:55:42

--- js/jquery.fancyboxStart.js
+++ js/jquery.fancyboxStart.js
@@ -1,3 +1,3 @@
-// build time:Sun Nov 24 2019 12:51:43 GMT+0800 (GMT+08:00)
+// build time:Sat Dec 07 2019 14:55:40 GMT+0800 (GMT+08:00)
 $(document).ready(function(){FancyBoxStart()});function FancyBoxStart(){$("img").each(function(){var a=$(this);var t=a.parent("a");if(t.length==0){var e=a.attr("src");var r=e.lastIndexOf("?");if(r!=-1){e=e.substring(0,r)}t=a.wrap('<a href="'+e+'"></a>').parent("a")}t.attr("data-fancybox","images")});$().fancybox({selector:'[data-fancybox="images"]',thumbs:false,hash:true,loop:false,fullScreen:false,slideShow:false,protect:true})}
 //rebuild by neat 
--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sun Nov 24 2019 12:51:43 GMT+0800 (GMT+08:00)
+// build time:Sat Dec 07 2019 14:55:40 GMT+0800 (GMT+08:00)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 

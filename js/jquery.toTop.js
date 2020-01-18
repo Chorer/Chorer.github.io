31b0862a3b9c6aa96d5b09a7d3eeeebb0466d917
Chorer <zcl82806932@163.com>
2020-01-18 21:16:24 +0800

    Site updated: 2020-01-18 21:16:12

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sat Jan 18 2020 20:18:48 GMT+0800 (GMT+08:00)
+// build time:Sat Jan 18 2020 21:04:55 GMT+0800 (GMT+08:00)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 
--- js/post-except.js
+++ js/post-except.js
@@ -1,3 +1,3 @@
-// build time:Sat Jan 18 2020 20:18:48 GMT+0800 (GMT+08:00)
+// build time:Sat Jan 18 2020 21:04:55 GMT+0800 (GMT+08:00)
 var cards=document.querySelectorAll(".post-content");for(var j=0;j<cards.length;j++){if(cards[j].querySelectorAll("img").length>0){cards[j].classList.add("post-content-padding")}else{cards[j].classList.add("post-content-noimg")}if(cards[j].querySelectorAll("blockquote").length>0){cards[j].classList.add("post-content-quote")}if(!cards[j].querySelectorAll("p").length){if(cards[j].children[0].children[0]){cards[j].children[0].children[0].style.display="block"}}}
 //rebuild by neat 

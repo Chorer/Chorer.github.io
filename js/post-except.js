53ed3981bc8e46a6e098438d69bba537f9d2f1ab
Chorer <zcl82806932@163.com>
2021-01-20 10:22:26 +0800

    Site updated: 2021-01-20 10:22:21

--- js/post-except.js
+++ js/post-except.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 19 2021 23:57:53 GMT+0800 (GMT+08:00)
+// build time:Wed Jan 20 2021 10:22:19 GMT+0800 (GMT+08:00)
 var cards=document.querySelectorAll(".post-content");for(var j=0;j<cards.length;j++){if(cards[j].querySelectorAll("img").length>0){cards[j].classList.add("post-content-padding")}else{cards[j].classList.add("post-content-noimg")}if(cards[j].querySelectorAll("blockquote").length>0){cards[j].classList.add("post-content-quote")}if(!cards[j].querySelectorAll("p").length){if(cards[j].children[0].children[0]){cards[j].children[0].children[0].style.display="block"}}}
 //rebuild by neat 

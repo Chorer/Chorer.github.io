45e62f92a39b7d963625bfa38dbf421201273b44
Chorer <zcl82806932@163.com>
2021-06-13 13:01:07 +0800

    Site updated: 2021-06-13 13:00:49

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Wed Jan 20 2021 10:49:33 GMT+0800 (GMT+08:00)
+// build time:Sun Jun 13 2021 13:00:41 GMT+0800 (GMT+08:00)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 

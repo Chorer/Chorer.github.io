7b0852b339151b6406a7b62aa803ed3828f1f147
Chorer <zcl82806932@163.com>
2021-09-05 14:29:46 +0800

    Site updated: 2021-09-05 14:29:41

--- js/IndexToTop.js
+++
@@ -1,3 +0,0 @@
-// build time:Sun Jun 13 2021 13:00:41 GMT+0800 (GMT+08:00)
-addBackToTop({diameter:56,backgroundColor:"#53b3fa",textColor:"#fff",scrollDuration:300});
-//rebuild by neat 
--- js/jquery.toTop.js
+++
@@ -1,3 +0,0 @@
-// build time:Sun Jun 13 2021 13:00:41 GMT+0800 (GMT+08:00)
-$(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
-//rebuild by neat 

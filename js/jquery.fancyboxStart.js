faafd3c353d4bb44c2f52722c44270f59099e7d2
Chorer <zcl82806932@163.com>
2021-09-05 14:33:05 +0800

    Site updated: 2021-09-05 14:33:00

---
+++ js/jquery.fancyboxStart.js
@@ -0,0 +1,3 @@
+// build time:Sun Sep 05 2021 14:32:59 GMT+0800 (中国标准时间)
+$(document).ready(function(){FancyBoxStart()});function FancyBoxStart(){$("img").each(function(){var a=$(this);var t=a.parent("a");if(t.length==0){var e=a.attr("src");var r=e.lastIndexOf("?");if(r!=-1){e=e.substring(0,r)}t=a.wrap('<a href="'+e+'"></a>').parent("a")}t.attr("data-fancybox","images")});$().fancybox({selector:'[data-fancybox="images"]',thumbs:false,hash:true,loop:false,fullScreen:false,slideShow:false,protect:true})}
+//rebuild by neat 

cecdfa22a1b59477599ab78d831c6942367f5940
Chorer <zcl82806932@163.com>
2021-09-05 16:08:47 +0800

    Site updated: 2021-09-05 16:08:42

--- js/IndexToTop.js
+++ js/IndexToTop.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 05 2021 14:32:59 GMT+0800 (中国标准时间)
+// build time:Sun Sep 05 2021 16:01:29 GMT+0800 (中国标准时间)
 addBackToTop({diameter:56,backgroundColor:"#53b3fa",textColor:"#fff",scrollDuration:300});
 //rebuild by neat 

109e5d35476d5653ddc6cfa2349097c008cf2fd6
Chorer <zcl82806932@163.com>
2021-11-28 17:40:43 +0800

    Site updated: 2021-11-28 17:40:22

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 05 2021 18:35:19 GMT+0800 (中国标准时间)
+// build time:Sun Nov 28 2021 17:40:14 GMT+0800 (中国标准时间)
 $(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
 //rebuild by neat 
--- js/post-except.js
+++ js/post-except.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 05 2021 18:35:19 GMT+0800 (中国标准时间)
+// build time:Sun Nov 28 2021 17:40:14 GMT+0800 (中国标准时间)
 var cards=document.querySelectorAll(".post-content");for(var j=0;j<cards.length;j++){if(cards[j].querySelectorAll("img").length>0){cards[j].classList.add("post-content-padding")}else{cards[j].classList.add("post-content-noimg")}if(cards[j].querySelectorAll("blockquote").length>0){cards[j].classList.add("post-content-quote")}if(!cards[j].querySelectorAll("p").length){if(cards[j].children[0].children[0]){cards[j].children[0].children[0].style.display="block"}}}
 //rebuild by neat 
